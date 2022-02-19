a06caf3b9a4220131f575da2706f6a8ea34abbb5
Marcel Groeneweg <mgroeneweg@users.noreply.github.com>
2022-02-19 15:14:49 +0100

    Reverted to widget tools 8.18.3 to make widget work again

--- typings/NativeImageViewerProps.d.ts
+++ typings/NativeImageViewerProps.d.ts
@@ -3,26 +3,22 @@
  * WARNING: All changes made to this file will be overwritten
  * @author Mendix UI Content Team
  */
-import { CSSProperties } from "react";
 import { ActionValue, DynamicValue, EditableValue, NativeImage } from "mendix";
-import { Big } from "big.js";
 
 export interface NativeImageViewerProps<Style> {
     name: string;
     style: Style[];
     imageToView: DynamicValue<NativeImage>;
-    imageWidthAttr: EditableValue<Big>;
-    imageHeightAttr: EditableValue<Big>;
+    imageWidthAttr: EditableValue<BigJs.Big>;
+    imageHeightAttr: EditableValue<BigJs.Big>;
     showModal: boolean;
     onCloseAction?: ActionValue;
 }
 
 export interface NativeImageViewerPreviewProps {
-    className: string;
+    class: string;
     style: string;
-    styleObject?: CSSProperties;
-    readOnly: boolean;
-    imageToView: { type: "static"; imageUrl: string; } | { type: "dynamic"; entity: string; } | null;
+    imageToView: string;
     imageWidthAttr: string;
     imageHeightAttr: string;
     showModal: boolean;
