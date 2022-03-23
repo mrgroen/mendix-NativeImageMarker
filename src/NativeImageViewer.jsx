89dd13edd9ee58035704fb64d5e3c485302b421a
Marcel Groeneweg <mgroeneweg@users.noreply.github.com>
2022-03-23 13:19:02 +0100

    Initial zoom

--- src/NativeImageViewer.jsx
+++ src/NativeImageViewer.jsx
@@ -1,10 +1,12 @@
-import { PixelRatio, View } from "react-native";
+import { PixelRatio, View, useWindowDimensions } from "react-native";
 import { Image } from "mendix/components/native/Image";
 import { ReactNativeZoomableView } from "@openspacelabs/react-native-zoomable-view";
 
 import { createElement } from "react";
 
 export function NativeImageViewer({ imageToView, imageWidthAttr, imageHeightAttr, onCloseAction }) {
+    const window = useWindowDimensions();
+
     if (!imageToView || imageToView.status !== "available" || !imageToView.value) {
         return null;
     }
@@ -18,13 +20,21 @@ export function NativeImageViewer({ imageToView, imageWidthAttr, imageHeightAttr
     const pixelDensity = PixelRatio.get();
     imageWidth = PixelRatio.roundToNearestPixel(imageWidth / pixelDensity);
     imageHeight = PixelRatio.roundToNearestPixel(imageHeight / pixelDensity);
+    let zoomRatio = 0;
+    if (window.height > window.width) {
+        // Portrait
+        zoomRatio = window.width / imageWidth;
+    } else {
+        // Landscape
+        zoomRatio = window.height / imageHeight;
+    }
 
     return (
         <View style={{ flex: 1 }}>
             <ReactNativeZoomableView
                 maxZoom={30}
-                minZoom={0.1}
-                initialZoom={0.5}
+                minZoom={zoomRatio}
+                initialZoom={zoomRatio}
                 contentWidth={imageWidth}
                 contentHeight={imageHeight}
                 onSingleTap={() => {
